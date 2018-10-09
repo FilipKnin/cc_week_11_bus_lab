1cc344257e9c84aa51a82aece39bd4c106462d5f
Filip Kuszenin <filipkuszenin@gmail.com>
2018-10-09 12:44:55 +0100

    Bus.getDestination created and tested

--- src/test/java/BusTest.java
+++ src/test/java/BusTest.java
@@ -1,2 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BusTest {
+
+    Bus bus;
+
+    @Before
+    public void before(){
+        bus = new Bus("destination", 20);
+    }
+
+    @Test
+    public void hasDestination() {
+        assertEquals("destination", bus.getDestination());
+    }
+
+
 }
