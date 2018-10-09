fdd441c5b8c56bcde66653936610ab665754940e
Filip Kuszenin <filipkuszenin@gmail.com>
2018-10-09 13:13:04 +0100

    BusStop.getName created and tested

---
+++ src/test/java/BusStopTest.java
@@ -0,0 +1,23 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BusStopTest {
+
+    BusStop busStop;
+    Person person;
+
+
+    @Before
+    public void before() {
+        busStop = new BusStop("name");
+        person = new Person();
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("name", busStop.getName());
+    }
+
+}
